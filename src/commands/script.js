9a61389b7336618ba31fe3ca31977768b61b4390
Richard Yu <vipzhicheng@gmail.com>
2018-05-11 08:34:31 +0800

    adjust code

---
+++ src/commands/script.js
@@ -0,0 +1,13 @@
+
+exports.command = 'script [file]'
+exports.alias = 'scr'
+exports.desc = 'Execute a script'
+
+exports.builder = function (yargs) {
+}
+
+exports.handler = function (argv) {
+  // TODO
+  // Add pre and post hook
+  console.log('Start to draw your dream code!')
+}
