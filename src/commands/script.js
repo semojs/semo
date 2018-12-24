b90d13b5cd66ff1c71e229dd12c2a5ef2e164eee
vipzhicheng <vipzhicheng@gmail.com>
2018-12-24 15:35:26 +0800

    adjust script file find logic

--- src/commands/script.js
+++ src/commands/script.js
@@ -1,4 +1,5 @@
 const co = require('co')
+const fs = require('fs')
 const path = require('path')
 const { Utils } = require('../../')
 
@@ -11,6 +12,11 @@ exports.builder = function (yargs) {}
 exports.handler = function (argv) {
   co(function * () {
     const components = Utils.invokeHook('components')
-    yield require(path.resolve(process.cwd(), argv.file))(argv, () => components)
+    let filePath = argv.file
+    if (!fs.existsSync(filePath)) {
+      filePath = path.resolve(process.cwd(), argv.file)
+    }
+
+    yield require(filePath)(argv, () => components)
   }).catch(e => Utils.error(e.stack))
 }
