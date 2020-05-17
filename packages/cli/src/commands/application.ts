d537899c0ccf59bafd60a942812e00a2bc6b7ce5
Richard Yu <vipzhicheng@gmail.com>
2020-05-17 14:59:39 +0800

    fix: rename application command to app

--- packages/cli/src/commands/application.ts
+++ packages/cli/src/commands/application.ts
@@ -1,13 +1,13 @@
 import { Utils } from '@semo/core'
 
-export const command = 'application'
+export const command = 'app'
 export const desc = 'Application command namespace.'
-export const aliases = 'app'
+export const aliases = 'application'
 
 export const builder = function(yargs) {
   const argv: any = Utils.getInternalCache().get('argv')
   const scriptName = argv.scriptName || 'semo'
-  Utils.extendSubCommand('application', scriptName, yargs, __dirname)
+  Utils.extendSubCommand('app', scriptName, yargs, __dirname)
 }
 
 export const handler = function(argv) {}
