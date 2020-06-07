b1062271ac0ff0a5e01db8c1ffef9c59ea65ad42
Richard Yu <vipzhicheng@gmail.com>
2020-06-07 22:06:28 +0800

    fix: hook not work using run command

--- packages/semo-plugin-hook/src/commands/hook/info.ts
+++ packages/semo-plugin-hook/src/commands/hook/info.ts
@@ -12,9 +12,13 @@ export const builder = function (yargs: any) {
 }
 
 export const handler = async function (argv: any) {
+  if (!argv.hook) {
+    Utils.error('A hook is required.')
+  }
+  
   if (argv.hook.startsWith('hook_')) {
     argv.hook = argv.hook.substring(5)
   }
-  const hookInfo = await Utils.invokeHook(argv.hook, { mode: 'group' })
+  const hookInfo = await Utils.invokeHook(argv.hook, { mode: 'group' }, argv)
   Utils.log(hookInfo)
 }
