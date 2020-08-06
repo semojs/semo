bc45a94c0bd5e5e860e2ed29909ef4f2381ce8ca
Richard Yu <vipzhicheng@gmail.com>
2020-08-06 23:20:55 +0800

    fix: adjust run command args and options

--- packages/cli/src/commands/run.ts
+++ packages/cli/src/commands/run.ts
@@ -6,42 +6,42 @@ const debug = Utils.debug('semo:')
 export const noblank = true // run call another command so disable one blank line
 export const plugin = 'semo'
 export const disabled = false // Set to true to disable this command temporarily
-export const command = 'run <PLUGIN>'
+export const command = 'run <plugin>'
 export const desc = 'Run any plugin command directly'
 // export const aliases = ''
 // export const middleware = (argv) => {}
 
 export const builder = function (yargs: any) {
-  yargs.option('UPGRADE', { describe: 'Force upgrade plugin cache', alias: 'UP' })
-  yargs.option('SCOPE', { default: '', describe: 'Set plugin npm scope' })
-  yargs.option('DEP', { describe: 'Set plugin dependent plugins' })
+  yargs.option('force', { describe: 'Force upgrade plugin cache', alias: 'F' })
+  yargs.option('scope', { default: '', describe: 'Set plugin npm scope' })
+  yargs.option('with', { describe: 'Set plugin dependent plugins' })
   // yargs.commandDir('run')
 }
 
 export const handler = async function (argv: any) {
   const scriptName= argv.scriptName || 'semo'
-  if (argv.PLUGIN.indexOf(`${scriptName}-plugin-`) === -1) {
-    argv.PLUGIN = `${scriptName}-plugin-${argv.PLUGIN}`
+  if (argv.plugin.indexOf(`${scriptName}-plugin-`) === -1) {
+    argv.plugin = `${scriptName}-plugin-${argv.plugin}`
   }
 
-  if (argv.SCOPE) {
-    argv.PLUGIN = `@${argv.SCOPE}/${argv.PLUGIN}`
+  if (argv.scope) {
+    argv.plugin = `@${argv.scope}/${argv.plugin}`
   }
 
-  argv.DEP = argv.DEP ? Utils._.castArray(argv.DEP) : []
+  argv.with = argv.with ? Utils._.castArray(argv.with) : []
 
-  if (argv.DEP && argv.DEP.length > 0) {
-    argv.DEP.forEach(dep => {
+  if (argv.with && argv.with.length > 0) {
+    argv.with.forEach(dep => {
       if (dep.indexOf(`${scriptName}-plugin-`) === -1) {
         dep = `${scriptName}-plugin-${dep}`
       }
     
-      if (argv.SCOPE) {
-        dep = `@${argv.SCOPE}/${dep}`
+      if (argv.scope) {
+        dep = `@${argv.scope}/${dep}`
       }
   
       try {
-        Utils.installPackage(dep, 'run-plugin-cache', true, argv.UPGRADE)
+        Utils.installPackage(dep, 'run-plugin-cache', true, argv.force)
       } catch (e) {
         Utils.error(`Plugin ${dep} not found or entry not exist`)
       }
@@ -49,9 +49,9 @@ export const handler = async function (argv: any) {
   }
 
   try {
-    Utils.installPackage(argv.PLUGIN, 'run-plugin-cache', true, argv.UPGRADE)
+    Utils.installPackage(argv.plugin, 'run-plugin-cache', true, argv.force)
   } catch (e) {
-    Utils.error(`Plugin ${plugin} not found or entry not exist`)
+    Utils.error(`Plugin ${argv.plugin} not found or entry not exist`)
   }
 
   // Pass SEMO_PLUGIN_DIR
@@ -59,7 +59,7 @@ export const handler = async function (argv: any) {
   let runPluginDir = path.resolve(String(process.env.HOME), `.${scriptName}`, 'run-plugin-cache', 'node_modules')
 
   // Populate command string
-  let pluginShort = argv.PLUGIN.substring(argv.PLUGIN.indexOf(`${scriptName}-plugin-`) + `${scriptName}-plugin-`.length)
+  let pluginShort = argv.plugin.substring(argv.plugin.indexOf(`${scriptName}-plugin-`) + `${scriptName}-plugin-`.length)
   let command = argv._
   command.shift()
   if (command.length === 0) {
