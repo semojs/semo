e16ea6e18bd4b0fe75c46be2cbe00c5c08d8fb02
Richard Yu <vipzhicheng@gmail.com>
2020-09-02 18:42:25 +0800

    feat: add clear console api function

--- packages/core/src/common/utils.ts
+++ packages/core/src/common/utils.ts
@@ -1950,9 +1950,9 @@ const consoleReader = (content: string, opts: { plugin?: string, identifier?: st
  * Clear console
  */
 const clearConsole = () => {
-  process.stdout.write(
+  process.stdout.isTTY && process.stdout.write(
     process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
-  );
+  )
 }
 
 /**
