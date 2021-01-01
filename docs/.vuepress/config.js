7ce9b4d788c5f8c828c400bd049df10805dd94c8
Richard Yu <vipzhicheng@gmail.com>
2021-01-01 19:38:33 +0800

    doc: add part en docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,58 +1,118 @@
 const host = process.env.NODE_ENV === 'development' ? 'localhost:8080' : 'semo.js.org'
 
 module.exports = {
-  title: 'Semo',
-  description: '一个Node项目命令行开发规范',
   base: '/',
   dest: 'public',
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+      title: 'Semo',
+      description: '一个Node项目命令行开发规范'
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'Semo',
+      description: 'A Node.js CLI building rules.'
+    }
+  },
   themeConfig: {
     repo: 'semojs/semo',
     repoLabel: 'Github',
     editLinks: true,
-    editLinkText: '帮助改进此页文档',
     docsDir: 'docs',
     displayAllHeaders: false,
-    nav: [
-      { text: '首页', link: '/'},
-      { text: '指南', link: '/guide/'},
-      { text: '用法', link: '/usage/'},
-      { text: '参考', link: `http://${host}/typedoc/`}
-    ],
-    sidebarDepth: 1,
-    sidebar: [
-      {
-        title: '基础',
-        collapsable: false,
-        children: [
-          '/guide/',
-          '/guide/quickstart/',
-          '/guide/core-commands/',
-          '/guide/custom-commands/',
-          '/guide/config/',
-          '/guide/hook/',
-          '/guide/plugin/',
+    locales: {
+      '/': {
+        editLinkText: '帮助改进此页文档',
+        nav: [
+          { text: '首页', link: '/'},
+          { text: '指南', link: '/guide/'},
+          { text: '用法', link: '/usage/'},
+          { text: '参考', link: `http://${host}/typedoc/`}
+        ],
+        sidebarDepth: 1,
+        sidebar: [
+          {
+            title: '基础',
+            collapsable: false,
+            children: [
+              '/guide/',
+              '/guide/quickstart/',
+              '/guide/core-commands/',
+              '/guide/custom-commands/',
+              '/guide/config/',
+              '/guide/hook/',
+              '/guide/plugin/',
+            ]
+          },
+          {
+            title: '用法',
+            collapsable: false,
+            children: [
+              '/usage/',
+              '/usage/plugin/',
+              '/usage/integration/',
+              '/usage/solution/',
+              '/usage/distribution/',
+            ]
+          },
+          {
+            title: '社区',
+            collapsable: false,
+            children: [
+              '/community/contrib/',
+              '/community/qa/',
+            ]
+          }
         ]
-      },
-      {
-        title: '用法',
-        collapsable: false,
-        children: [
-          '/usage/',
-          '/usage/plugin/',
-          '/usage/integration/',
-          '/usage/solution/',
-          '/usage/distribution/',
-        ]
-      },
-      {
-        title: '社区',
-        collapsable: false,
-        children: [
-          '/community/contrib/',
-          '/community/qa/',
+      }, 
+      '/en/': {
+        editLinkText: 'Help improve this page',
+        nav: [
+          { text: 'Home', link: '/en/'},
+          { text: 'Guide', link: '/guide/'},
+          { text: 'Usage', link: '/usage/'},
+          { text: 'Reference', link: `http://${host}/typedoc/`}
+        ],
+        sidebarDepth: 1,
+        sidebar: [
+          {
+            title: 'Guide',
+            collapsable: false,
+            children: [
+              '/guide/',
+              '/guide/quickstart/',
+              '/guide/core-commands/',
+              '/guide/custom-commands/',
+              '/guide/config/',
+              '/guide/hook/',
+              '/guide/plugin/',
+            ]
+          },
+          {
+            title: 'Usage',
+            collapsable: false,
+            children: [
+              '/usage/',
+              '/usage/plugin/',
+              '/usage/integration/',
+              '/usage/solution/',
+              '/usage/distribution/',
+            ]
+          },
+          {
+            title: 'Community',
+            collapsable: false,
+            children: [
+              '/community/contrib/',
+              '/community/qa/',
+            ]
+          }
         ]
       }
-    ]
+    }
   }
 
 }
---
+++ docs/en/README.md
@@ -0,0 +1,29 @@
+---
+home: true
+actionText: Get started →
+actionLink: /guide/quickstart/
+features:
+- title: Consistency
+  details: No matter what framework used and how structured, you can use this package to implement consistent command line scripts.
+- title: Flexibility
+  details: You can extend by plugins, you can override commands, config, and you can hook with core or other plugins.
+- title: Efficiency
+  details: The command rule is simple, so it easy to use, if you use it frequently, it can dramatically improve you performant.
+footer: Enterprise level Node CLI building rules
+---
+
+```bash
+# Install globally in your local machine.
+npm install -g @semo/cli
+semo help
+
+# Install to your project
+cd YOUR_PROJECT
+npm install @semo/cli
+semo init
+semo generate command test
+
+# Use application to reduce first level commands
+npm install semo-plugin-application
+semo generate command application/test --extend=application
+```
