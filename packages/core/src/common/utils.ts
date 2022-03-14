80e2e92e392a801773535c1e5636fd655439841f
vipzhicheng <vipzhicheng@gmail.com>
2022-03-14 15:03:44 +0800

    fix: debug

--- packages/core/src/common/utils.ts
+++ packages/core/src/common/utils.ts
@@ -2091,7 +2091,7 @@ const clearConsole = () => {
  * @module Utils
  */
 export const Utils = {
-  test: 5,
+  test: 6,
   // npm packages
   /** [lodash](https://www.npmjs.com/package/lodash) reference, check [doc](https://lodash.com/docs). */
   _,
